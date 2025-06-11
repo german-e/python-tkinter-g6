e319c4389271f842879c5157ec725a7340e364d0
german-e <german.espindola@gmail.com>
2025-06-10 21:25:23 -0300

    Estructura inicial del proyecto

---
+++ main.py
@@ -0,0 +1,6 @@
+from ui.pantalla_inicio import PantallaInicio
+import tkinter as tk
+
+if __name__ == "__main__":
+    app = PantallaInicio()
+    app.mainloop()
---
+++ ui/pantalla_inicio.py
@@ -0,0 +1,8 @@
+import tkinter as tk
+
+class PantallaInicio(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Sistema de gestión Escolar")
+        self.geometry("600x400")
+        
